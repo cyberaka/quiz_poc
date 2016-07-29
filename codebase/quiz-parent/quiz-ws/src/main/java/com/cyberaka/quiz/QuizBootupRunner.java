432053829bd83b7b373513c3e54f5f8925d8ba29
Abhinav Anand <cyberaka@gmail.com>
2016-07-29 13:39:39 +0530

    Unwanted stream usage removed.

--- codebase/quiz-parent/quiz-ws/src/main/java/com/cyberaka/quiz/QuizBootupRunner.java
+++ codebase/quiz-parent/quiz-ws/src/main/java/com/cyberaka/quiz/QuizBootupRunner.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
