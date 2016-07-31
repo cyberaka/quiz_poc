5a1b5cc5103e75d323253e167b20be982279a04b
Abhinav Anand <cyberaka@gmail.com>
2016-07-31 10:49:49 +0530

    Sonar feedback committed.

--- codebase/quiz-parent/quiz-ws/src/main/java/com/cyberaka/quiz/QuizBootApplication.java
+++ codebase/quiz-parent/quiz-ws/src/main/java/com/cyberaka/quiz/QuizBootApplication.java
@@ -1,16 +1,33 @@
 package com.cyberaka.quiz;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * Entry point into the quiz boot application.
+ * 
+ * @author anindita
+ *
+ */
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.cyberaka.quiz.dao")
 @EntityScan(basePackages = "com.cyberaka.quiz.domain")
 @ComponentScan(basePackages = { "com.cyberaka.quiz", "com.cyberaka.quiz.rest", "com.cyberaka.quiz.dao", "com.cyberaka.quiz.service.impl" })
 public class QuizBootApplication {
+	Logger log = Logger.getLogger(getClass().getName());
+	
+	public QuizBootApplication() {
+		// Default constructor.
+	}
+	
+	public void info() {
+		log.info("Quiz Boot Applicaiton");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(QuizBootApplication.class, args);
 	}
