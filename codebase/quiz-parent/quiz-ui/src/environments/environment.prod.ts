396dfade95f4d42c902affd283787db028235c5b
Bhaskar <Suresh.Bhaskar@harman.com>
2023-03-24 19:58:03 +0530

    token are removed

--- codebase/quiz-parent/quiz-ui/src/environments/environment.prod.ts
+++ codebase/quiz-parent/quiz-ui/src/environments/environment.prod.ts
@@ -1,4 +1,28 @@
+import config from './../../auth_config.json';
+
+const { domain, clientId, authorizationParams: { audience }, apiUri, errorPath } = config as {
+  domain: string;
+  clientId: string;
+  authorizationParams: {
+    audience?: string;
+  },
+  apiUri: string;
+  errorPath: string;
+};
+
 export const environment = {
   production: true,
-  baseURL: 'http://170.187.238.150:7070/',
+  baseURL: apiUri,
+  auth: {
+    domain,
+    clientId,
+    authorizationParams: {
+      ...(audience && audience !== 'YOUR_API_IDENTIFIER' ? { audience } : null),
+      redirect_uri: window.location.origin,
+    },
+    errorPath,
+  },
+  httpInterceptor: {
+    allowedList: [`${apiUri}/*`],
+  },
 };
