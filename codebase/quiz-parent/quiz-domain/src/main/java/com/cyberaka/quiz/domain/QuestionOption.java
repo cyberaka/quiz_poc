9e5f5f4e5ccb7d62251a5f786e1e76ec492e85b3
Abhinav Anand <cyberaka@gmail.com>
2023-04-22 20:09:12 +0530

    Removed unwanted id.

--- codebase/quiz-parent/quiz-domain/src/main/java/com/cyberaka/quiz/domain/QuestionOption.java
+++ codebase/quiz-parent/quiz-domain/src/main/java/com/cyberaka/quiz/domain/QuestionOption.java
@@ -1,13 +1,9 @@
 package com.cyberaka.quiz.domain;
 
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Document("questions_option")
 public class QuestionOption {
-
-    @MongoId
-    private String questionOptionId;
 
     private String option;
 
@@ -19,14 +15,6 @@ public class QuestionOption {
     public QuestionOption(String option, String text) {
         this.option = option;
         this.text = text;
-    }
-
-    public String getQuestionOptionId() {
-        return questionOptionId;
-    }
-
-    public void setQuestionOptionId(String questionOptionId) {
-        this.questionOptionId = questionOptionId;
     }
 
     public String getOption() {
